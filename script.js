93dd8be923ca8431e459a148f847a1bb63599d10
kamaluddin-babar <kamaluddin.cse94@gmail.com>
2021-02-08 20:39:54 +0600

    assignment-5 final code for submission

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
+// adding event listener to search button.
 const searchBtn = document.getElementById("search-btn");
 searchBtn.addEventListener("click", function () {
     const foodName = document.getElementById("food-input").value;
-
     loadData(foodName[0]);
-
     document.getElementById("food-details-container").style.display = "none";
 });
 
-
+// load food data from mealdb api that matches with first character of food name.
 function loadData(firstChar) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${firstChar}`)
         .then(response => response.json())
         .then(data => {
-            // console.log(data);
-            // console.log(typeof (data));
             displayData(data);
         })
         .catch((error) => {
-            //console.error("got fucked");
-            const errorMessage = document.getElementById("error-message");
-            errorMessage.innerHTML = "<h3>Sorry,there is no such record found.</h3>";
-            const foodContainer = document.getElementById("food-container");
-            foodContainer.innerHTML = "";
-          });
+            showErrorMessage();
+        });
 }
+
+// display all foods that matches with query.
 function displayData(foodList) {
     const foodContainer = document.getElementById("food-container");
     foodContainer.innerHTML = "";
-    
+
     const errorMessage = document.getElementById("error-message");
     errorMessage.innerHTML = "";
 
     foodList.meals.forEach(food => {
-        //console.log(food.strMeal, food.strMealThumb);
         const singleFoodDiv = document.createElement("div");
         singleFoodDiv.className = "col";
         const foodInfo = `
@@ -43,55 +37,65 @@ function displayData(foodList) {
                 </div>
             </div>
         `;
-
-
         singleFoodDiv.innerHTML = foodInfo;
         foodContainer.appendChild(singleFoodDiv);
-
-
         singleFoodDiv.addEventListener("click", function () {
-            // console.log("single food is clicked");
-            displayFoodDetails(`${food.strMeal}`);
+            displayFoodDetails(`${food.idMeal}`);
         });
     });
 }
 
-function displayFoodDetails(foodName) {
-    //console.log(foodName);
+// this function load details information about a food from food list using mealdb api and parameter is idMeal.
+function displayFoodDetails(foodId) {
     const foodDetailContainer = document.getElementById("food-details-container");
     foodDetailContainer.style.display = "block";
     foodDetailContainer.innerHTML = "";
-
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`)
         .then(response => response.json())
-        .then(food => {
-            const url = food.meals[0].strMealThumb;
-            const name = food.meals[0].strMeal;
-            const ingredient1 = food.meals[0].strMeasure1 + " " + food.meals[0].strIngredient1;
-            const ingredient2 = food.meals[0].strMeasure2 + " " + food.meals[0].strIngredient2;
-            const ingredient3 = food.meals[0].strMeasure3 + " " + food.meals[0].strIngredient3;
-            const ingredient4 = food.meals[0].strMeasure4 + " " + food.meals[0].strIngredient4;
-            const ingredient5 = food.meals[0].strMeasure5 + " " + food.meals[0].strIngredient5;
-            // console.log(name, url, ingredient1,ingredient2,ingredient3,ingredient4,ingredient5);
-
-            const foodDetailDiv = document.createElement("div");
-            const foodInfo = `
-                <div class="card" style="width: 100%; border: none";">
-                    <img src="${url}" class="card-img-top p-3" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${name}</h5>
-                        <h6><b>Ingredients</b></h6>
-                    </div>
-                    <ul class="list-group list-group-flush mt-0 mb-0 pt-0 pb-0">
-                        <li class="list-group-item mt-0 pt-0" style="border: none";><i class="fas fa-check-circle list-color"></i> ${ingredient1}</li>
-                        <li class="list-group-item mt-0 pt-0" style="border: none";><i class="fas fa-check-circle list-color"></i> ${ingredient2}</li>
-                        <li class="list-group-item mt-0 pt-0" style="border: none";><i class="fas fa-check-circle list-color"></i> ${ingredient3}</li>
-                        <li class="list-group-item mt-0 pt-0" style="border: none";><i class="fas fa-check-circle list-color"></i> ${ingredient4}</li>
-                        <li class="list-group-item mt-0 pt-0" style="border: none";><i class="fas fa-check-circle list-color"></i> ${ingredient5}</li>
-                    </ul>
-                </div>
-                `;
-            foodDetailDiv.innerHTML = foodInfo;
-            foodDetailContainer.appendChild(foodDetailDiv);
+        .then(food => foodIngredientInfo(food.meals))
+        .catch(error => {
+            showErrorMessage();
         })
+}
+
+// this function displays details information of a food.
+function foodIngredientInfo(foodInfo) {
+    console.log(foodInfo);
+    const foodDetailContainer = document.getElementById("food-details-container");
+    foodInfo.forEach(food => {
+        const url = food.strMealThumb;
+        const name = food.strMeal;
+        const ingredient1 = food.strMeasure1 + " " + food.strIngredient1;
+        const ingredient2 = food.strMeasure2 + " " + food.strIngredient2;
+        const ingredient3 = food.strMeasure3 + " " + food.strIngredient3;
+        const ingredient4 = food.strMeasure4 + " " + food.strIngredient4;
+        const ingredient5 = food.strMeasure5 + " " + food.strIngredient5;
+        const foodDetailDiv = document.createElement("div");
+        const foodInfo = `
+        <div class="card" style="width: 100%; border: none";">
+            <img src="${url}" class="card-img-top p-3" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <h6><b>Ingredients</b></h6>
+            </div>
+            <ul class="list-group list-group-flush mt-0 mb-0 pt-0 pb-0">
+                <li class="list-group-item mt-0 pt-0" style="border: none";><i class="fas fa-check-circle list-color"></i> ${ingredient1}</li>
+                <li class="list-group-item mt-0 pt-0" style="border: none";><i class="fas fa-check-circle list-color"></i> ${ingredient2}</li>
+                <li class="list-group-item mt-0 pt-0" style="border: none";><i class="fas fa-check-circle list-color"></i> ${ingredient3}</li>
+                <li class="list-group-item mt-0 pt-0" style="border: none";><i class="fas fa-check-circle list-color"></i> ${ingredient4}</li>
+                <li class="list-group-item mt-0 pt-0" style="border: none";><i class="fas fa-check-circle list-color"></i> ${ingredient5}</li>
+            </ul>
+        </div>
+        `;
+        foodDetailDiv.innerHTML = foodInfo;
+        foodDetailContainer.appendChild(foodDetailDiv);
+    });
+}
+
+// this function display error message when any error occur during loading data from mealdb api
+function showErrorMessage() {
+    const errorMessage = document.getElementById("error-message");
+    errorMessage.innerHTML = "<h3 class='text-danger'>Sorry,there is no such record found.</h3>";
+    const foodContainer = document.getElementById("food-container");
+    foodContainer.innerHTML = "";
 }
